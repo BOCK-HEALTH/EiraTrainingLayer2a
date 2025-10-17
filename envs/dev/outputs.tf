190e297ef42ca38a3191c0e0eb774014c4030fcf
Samuel-0316 <samuelsrujanb@gmail.com>
2025-10-17 19:08:54 +0530

    Initial commit of Task 2 project

---
+++ envs/dev/outputs.tf
@@ -0,0 +1,35 @@
+
+output "instance_id_1" {
+  value = module.ec2_instance_1.instance_id
+}
+output "public_ip_1" {
+  value = module.ec2_instance_1.public_ip
+}
+
+output "instance_id_2" {
+  value = module.ec2_instance_2.instance_id
+}
+output "public_ip_2" {
+  value = module.ec2_instance_2.public_ip
+}
+
+output "instance_id_3" {
+  value = module.ec2_instance_3.instance_id
+}
+output "public_ip_3" {
+  value = module.ec2_instance_3.public_ip
+}
+
+output "instance_id_4" {
+  value = module.ec2_instance_4.instance_id
+}
+output "public_ip_4" {
+  value = module.ec2_instance_4.public_ip
+}
+
+output "instance_id_5" {
+  value = module.ec2_instance_5.instance_id
+}
+output "public_ip_5" {
+  value = module.ec2_instance_5.public_ip
+}
